04e498dae18d8ef17e87ba8f91d520da8fcceba9
BrandonLimKee <78452249+BrandonLimKee@users.noreply.github.com>
2021-04-15 00:01:26 +0100

    Added converter to the program class

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,5 +1,4 @@
-﻿using ConsoleAppProject.App03;
-using ConsoleAppProject.Helpers;
+﻿using ConsoleAppProject.App01;
 using System;
 
 namespace ConsoleAppProject
@@ -21,15 +20,8 @@ namespace ConsoleAppProject
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
 
-            // Using an extension method for each enumeration
-            Console.WriteLine("Using MyEnum Extension Method!\n");
-            Console.WriteLine("MyEnum Value = " + MyEnum.FirstValue);
-            Console.WriteLine("MyEnum Friendly Value = " + MyEnum.FirstValue.EnumValue());
-            Console.WriteLine();
-
-            // Using an extension method for any enumeration
-            StudentGrades grades = new StudentGrades();
-            grades.TestGradesEnumeration();
+            DistanceConverter converter = new DistanceConverter();
+            converter.Run();
         }
     }
 }
